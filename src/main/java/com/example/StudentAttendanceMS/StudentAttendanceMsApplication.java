f7081da0e0c8255acc0a210f8b5a589ca8097188
dipti  sawant <diptisawant2023@gmail.com>
2023-04-04 21:40:46 +0530

    Initial commit

---
+++ src/main/java/com/example/StudentAttendanceMS/StudentAttendanceMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentAttendanceMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentAttendanceMS/StudentAttendanceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentAttendanceMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
